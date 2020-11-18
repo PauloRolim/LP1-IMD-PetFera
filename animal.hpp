5a586f14d22da4b4a7f0f86ec77e7a446f69b2ae
andrejr95 <50457552+andrejr95@users.noreply.github.com>
2020-11-18 18:16:24 -0300

    Update animal.hpp
    atualizando classe animal - adicionando classe , especie , tratador_responcavel e vet_responcavel

--- animal.hpp
+++ animal.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <iostream>
-#include <string>
+#include <string> 
 
 enum classificacaoTipo
 {
@@ -16,16 +16,31 @@ private:
     
     classificacaoTipo tipo;
     std::string nome_do_proprietario;
+    std::string classe;
+    std::string especie;
+    std::string tratador_responcavel;
+    std::string vet_responcavel;
 
 public:
-    animal(classificacaoTipo tipo, std::string nome_do_proprietario);
+    animal(classificacaoTipo tipo, std::string nome_do_proprietario, std::string classe,std::string especie, 
+           std::string tratador_responcavel, std::string vet_responcavel);
+    
     ~animal();
 
     std::string getNome_do_proprietario() const;
+    std::string getEspecie() const;
+    std::string getClasse() const;
+    std::string getTratador_responcavel() const;
+    std::string getVet_responcavel() const;
     void setNome_do_proprietario(std::string nome_do_proprietario);
+    void setClasse(std::string classe);
+    void setEspecie(std::string especie);
+    void setTratador_responcavel(std::string tratador_responcavel);
+    void setVet_responcavel(std::string vet_responcavel);
 };
 
-animal::animal(classificacaoTipo tipo, std::string nome_do_proprietario)
+animal::animal(classificacaoTipo tipo, std::string nome_do_proprietario, std::string classe, 
+               std::string especie, std::string tratador_responcavel, std::string vet_responcavel)
 {
 }
 
@@ -41,4 +56,46 @@ animal::getNome_do_proprietario() const {
 void 
 animal::setNome_do_proprietario(std::string nome_do_proprietario){
     this -> nome_do_proprietario = nome_do_proprietario;
+}
+
+std::string 
+animal::getClasse() const {
+    return this -> classe;
+}
+
+void 
+animal::setClasse(std::string classe){
+    this -> classe = classe;
+}
+
+
+std::string 
+animal::getEspecie() const {
+    return this -> especie;
+}
+
+void 
+animal::setEspecie(std::string especie){
+    this -> especie = especie;
+}
+
+
+std::string 
+animal::getTratador_responcavel() const {
+    return this -> tratador_responcavel;
+}
+
+void 
+animal::setTratador_responcavel(std::string tratador_responcavel){
+    this -> tratador_responcavel = tratador_responcavel;
+}
+
+std::string 
+animal::getVet_responcavel() const {
+    return this -> vet_responcavel;
+}
+
+void 
+animal::setVet_responcavel(std::string vet_responcavel){
+    this -> vet_responcavel = vet_responcavel;
 }
