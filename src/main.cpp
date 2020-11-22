7c6b430ede4f9fbb3b63ae3de6b4e1505dabed9e
PauloRolim <pauloerb@hotmail.com>
2020-11-21 22:15:31 -0300

    testes

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
+#include <iostream>
+
+#include "funcionario.hpp"
 #include "veterinario.hpp"
 #include "tratador.hpp"
 
 int main(){
 
-    veterinario* vet01 = new veterinario("0123", "01", "Paulo Jardel", 1045.15, "01/01/2020");
+    veterinario* vet01 = new veterinario();
+    tratador* trat01 = new tratador();
 
-    tratador* trat01 = new tratador("azul", "02", "Nailson Gomes", 600.50, "01/02/2020");
+    veterinario* vet02 = new veterinario("0220", "03", "G. Lima", 500.15, "01/01/2020");
 
-    std::cout << vet01->getNome() << std::endl;
+    vet01->setNome("Nailson Gomes");
+    vet01->setNumeroCRMV("0123");
 
-    std::cout << trat01->getNome() << std::endl;
+    trat01->setNome("Paulo Jardel");
+    trat01->setCorUniforme("verde");
+
+    std::cout << vet01->getNome() << " " << vet01->getNumeroCRMV() << std::endl;
+
+    std::cout << trat01->getNome() << " " << trat01->getCorUniforme() << std::endl;
+
+    std::cout << "Contratado em " << vet02->getAdmissao() << std::endl;
 
     return 0;
 }
