6e60c16974d4bc11016ccd05458155bd162e1884
PauloRolim <pauloerb@hotmail.com>
2020-11-23 10:24:02 -0300

    add vetor de tratadores e funções

--- include/loja_petfera.hpp
+++ include/loja_petfera.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "veterinario.hpp"
+#include "tratador.hpp"
 
 #define CAPACIDADE_MAX 10
 
@@ -12,11 +13,14 @@ private:
 
     int quadro_de_funcionarios = 0;
     veterinario* veterinarios[CAPACIDADE_MAX];
+    tratador* tratadores[CAPACIDADE_MAX];
 
 public:    
 
-    int getCapacidade();
+    int getQadro_de_funcionarios();
     void addVeterinario(veterinario* novo);
-    void removePeloId(std::string Id);
+    void addTratador(tratador* novo);
+    void remove_Veterinario_PeloId(std::string Id);
+    void remove_Tratador_PeloId(std::string Id);
     void listarVeterinarios();
 };
--- src/loja_petfera.cpp
+++ src/loja_petfera.cpp
@@ -3,7 +3,7 @@
 #include "loja_petfera.hpp"
 
 int 
-loja_petfera::getCapacidade(){
+loja_petfera::getQadro_de_funcionarios(){
     return this -> quadro_de_funcionarios;
 }
 
@@ -17,11 +17,10 @@ loja_petfera::addVeterinario(veterinario* novo){
         std::cout << "Capacidade maxia atingida!" << std::endl;
         std::cout << "Entre em contato com o administrador do sistema." << std::endl;
     }
-    
 }
 
 void 
-loja_petfera::removePeloId(std::string Id){
+loja_petfera::remove_Veterinario_PeloId(std::string Id){
     bool encontrou = false;
 
     for (int i = 0; i < this -> quadro_de_funcionarios; ++i)
@@ -50,5 +49,27 @@ loja_petfera::listarVeterinarios(){
         std::cout <<  "Id: " << veterinarios[i]->getId() <<
         " Nome: " << veterinarios[i]->getNome() << std::endl;
     }
-    
+}
+
+void 
+loja_petfera::remove_Tratador_PeloId(std::string Id){
+    bool encontrou = false;
+
+    for (int i = 0; i < this -> quadro_de_funcionarios; ++i)
+    {
+        if (this -> tratadores[i]->getId() == Id)
+        {
+            tratador* temp = tratadores[i];
+            tratadores[i] = tratadores[i+1];
+            tratadores[i+1] = temp;
+            encontrou = true;
+        }
+    }
+
+    if (encontrou == true)
+    {
+        delete tratadores[this->quadro_de_funcionarios];
+        this->quadro_de_funcionarios--;
+        std::cout << "Tratador removido!" << std::endl;
+    } 
 }
