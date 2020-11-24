93af5ea0316edd432eafbbb08f79475a644caced
Paulo Rolim <41303759+PauloRolim@users.noreply.github.com>
2020-11-24 09:45:30 -0300

    ajustes na classe animal

--- src/animal/Animal.cpp
+++ src/animal/Animal.cpp
@@ -1,68 +1,58 @@
-#include "Animal.hpp"
+#include "animal.hpp"
 
-Animal::Animal(
-    std::string classe,
-    std::string especie,
-    std::string nome,
-    double preco,
-    Tratador* tratador_responcavel,
-    Veterinario* vet_responcavel) {
-    this->setClasse(classe);
-    this->setEspecie(especie);
-    this->setNome(nome);
-    this->setpreco(preco);
-    this->setTratadorResponcavel(tratador_responcavel);
+animal::animal(){}
+
+animal::animal(std::string _classe, std::string especie, std::string id_pet,
+               std::string tratador_responcavel, std::string vet_responcavel, double preco, _sexo sexo, 
+               classificacaoRisco risco, alimentacao comida)
+{
 }
 
-Animal::~Animal() {
+animal::~animal()
+{
 }
 
-/* getters */ 
-std::string Animal::getClasse() const {
-    return this->classe;
+std::string 
+animal::get_classe() const {
+    return this -> _classe;
 }
 
-std::string Animal::getEspecie() const {
-    return this->especie;
+std::string
+animal::getEspecie() const {
+    return this -> especie;
 }
 
-std::string Animal::getNome() const {
-    return this->nome;
+std::string 
+animal::getId_pet() const {
+    return this -> id_pet;
 }
 
-double Animal::getPreco() const {
-    return this->preco;
+std::string
+animal::getTratador_responcavel() const {
+    return this -> tratador_responcavel;
 }
 
-Tratador* Animal::getTratadorResponcavel() const {
-    return this->tratador_responcavel;
+std::string 
+animal::getVet_responcavel() const {
+    return this -> vet_responcavel;
 }
 
-Veterinario* Animal::getVetResponcavel() const {
-    return this->vet_responcavel;
+double
+animal::getPreco() const {
+    return this -> preco;
 }
 
-/* setters */ 
-void Animal::setClasse(std::string classe) {
-    this->classe = classe;
+_sexo
+animal::getSexo() const{
+    return this -> sexo;
 }
 
-void Animal::setEspecie(std::string especie) {
-    this->especie = especie;
+classificacaoRisco
+animal:: getRisco() const{
+    return this -> risco;
 }
 
-void Animal::setNome(std::string nome) {
-    this->nome = nome;
-}
-
-void Animal::setPreco(double preco) {
-    this->preco = preco;
-}
-
-void Animal::setTratadorResponcavel(Tratador* tratador_responcavel) {
-    this->tratador_responcavel = tratador_responcavel;
-}
-
-void Animal::setVetResponcavel(Veterinario* vet_responcavel) {
-    this->vet_responcavel = vet_responcavel;
+alimentacao
+animal::getComida() const{
+    return this -> comida;
 }
