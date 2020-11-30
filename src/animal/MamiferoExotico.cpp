140efb7cd9c80e2f8cc60aad4b7408493857c3d5
PauloRolim <pauloerb@hotmail.com>
2020-11-29 21:09:01 -0300

    Implementação das classes mamifero e reptil exot

---
+++ src/animal/MamiferoExotico.cpp
@@ -0,0 +1,18 @@
+#include "../../include/animal/MamiferoExotico.hpp"
+
+MamiferoExotico::MamiferoExotico(
+    std::string classe,
+    std::string especie,
+    std::string nome,
+    double preco,
+    Tratador* tratador_responcavel,
+    Veterinario* vet_responcavel,
+    _sexo sexo,
+    _classificacaoRisco risco,
+    _alimentacao comida,
+    porte_do_mamifero porte_tipo, 
+    requer_gaiola gaiola_tipo,
+    std::string pais_de_origem
+)
+{
+}
---
+++ src/animal/ReptilExotico.cpp
@@ -0,0 +1,18 @@
+#include "../../include/animal/ReptilExotico.hpp"
+
+ReptilExotico::ReptilExotico(
+    std::string classe,
+    std::string especie,
+    std::string nome,
+    double preco,
+    Tratador* tratador_responcavel,
+    Veterinario* vet_responcavel,
+    _sexo sexo,
+    _classificacaoRisco risco,
+    _alimentacao comida,
+    tamanho_do_habitat tamanho_tipo, 
+    tipo_do_habitat habitat_tipo, 
+    requer_luz_uv luz_tipo,
+    std::string pais_de_origem)
+{
+}
