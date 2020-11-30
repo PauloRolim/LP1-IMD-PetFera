c57c09349a0b775574fec51fffc8444cc45b3dba
andrejr95 <50457552+andrejr95@users.noreply.github.com>
2020-11-30 15:41:20 -0300

    Create MamiferoNativo.hpp
    adicionando MamiferoNativo

---
+++ include/animal/MamiferoNativo.hpp
@@ -0,0 +1,25 @@
+#include "../../include/animal/Mamifero.hpp"
+#include "../../include/animal/Nativo.hpp"
+
+class MamiferoNativo: public Mamifero, public Nativo
+{
+protected:
+    
+public:
+    MamiferoNativo();
+    MamiferoNativo(
+        std::string classe,
+        std::string especie,
+        std::string nome,
+        double preco,
+        Tratador* tratador_responcavel,
+        Veterinario* vet_responcavel,
+        _sexo sexo,
+        _classificacaoRisco risco,
+        _alimentacao comida,
+        porte_do_mamifero porte_tipo, 
+        requer_gaiola gaiola_tipo,
+        std::string licenca_IBAMA
+    );
+    virtual ~MamiferoNativo();
+};
