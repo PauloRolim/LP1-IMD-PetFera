db1f073dce5fa0d4edb0e146f4e5e276b023f978
andrejr95 <50457552+andrejr95@users.noreply.github.com>
2020-11-30 15:55:04 -0300

    Create ReptilNativo.hpp

---
+++ include/animal/ReptilNativo.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "../../include/animal/Reptil.hpp"
+#include "../../include/animal/Nativo.hpp"
+
+class ReptilNativo: public Reptil, public Nativo
+{
+protected:
+ 
+public:
+    ReptilNativo();
+    ReptilNativo(
+        std::string classe,
+        std::string especie,
+        std::string nome,
+        double preco,
+        Tratador* tratador_responcavel,
+        Veterinario* vet_responcavel,
+        _sexo sexo,
+        _classificacaoRisco risco,
+        _alimentacao comida,
+        tamanho_do_habitat tamanho_tipo, 
+        tipo_do_habitat habitat_tipo, 
+        requer_luz_uv luz_tipo,
+        std::string licenca_IBAMA
+    );
+
+    virtual ~ReptilNativo();
+};
