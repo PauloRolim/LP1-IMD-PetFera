238b7a1feaa00ea410b5360015ca68ca9deb9c7a
PauloRolim <pauloerb@hotmail.com>
2020-12-01 19:28:08 -0300

    Ajustes no construtor

--- include/animal/AnfibioExotico.hpp
+++ include/animal/AnfibioExotico.hpp
@@ -10,19 +10,6 @@ protected:
     
 public:
     AnfibioExotico();
-    AnfibioExotico(
-        std::string classe,
-        std::string especie,
-        std::string nome,
-        double preco,
-        Tratador* tratador_responcavel,
-        Veterinario* vet_responcavel,
-        _sexo sexo,
-        _classificacaoRisco risco,
-        _alimentacao comida,
-        tamanho_do_habitat tamanho_tipo, 
-        tipo_do_habitat habitat_tipo,
-        std::string pais_de_origem
-    );
+    
     virtual ~AnfibioExotico();
 };
--- src/animal/AnfibioExotico.cpp
+++ src/animal/AnfibioExotico.cpp
@@ -1,18 +1,5 @@
 #include "../../include/animal/AnfibioExotico.hpp"
 
-AnfibioExotico::AnfibioExotico( 
-        std::string classe,
-        std::string especie,
-        std::string nome,
-        double preco,
-        Tratador* tratador_responcavel,
-        Veterinario* vet_responcavel,
-        _sexo sexo,
-        _classificacaoRisco risco,
-        _alimentacao comida,
-        tamanho_do_habitat tamanho_tipo, 
-        tipo_do_habitat habitat_tipo,
-        std::string pais_de_origem
-    )
-{
-}
+AnfibioExotico::AnfibioExotico(): Anfibio(){}
+    
+AnfibioExotico::~AnfibioExotico(){}
